473cac5e3f3421427cde290dca6c76563a6de411
caroline <pcmaunda@gmail.com>
2021-11-27 09:45:51 +0300

    added form

--- Quotes/src/app/app.module.ts
+++ Quotes/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
 import { HighlightDirective } from './highlight.directive';
 import { DateCountPipe } from './date-count.pipe';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
 
 @NgModule({
   declarations: [
@@ -14,11 +15,13 @@ import { DateCountPipe } from './date-count.pipe';
     QuoteComponent,
     QuoteDetailComponent,
     HighlightDirective,
-    DateCountPipe
+    DateCountPipe,
+    QuoteFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Quotes/src/app/quote-form/quote-form.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit,Output,EventEmitter } from '@angular/core';
+import { Quote } from '../quote';
+
+@Component({
+  selector: 'app-quote-form',
+  templateUrl: './quote-form.component.html',
+  styleUrls: ['./quote-form.component.css']
+})
+export class QuoteFormComponent implements OnInit {
+
+  newQuote = new Quote("","","",new Date());
+  @Output() addQuote = new EventEmitter<Quote>();
+  
+    submitQuote(){
+  this.addQuote.emit(this.newQuote);
+    }
+  
+
+  @Output()isComplete = new EventEmitter<boolean>();
+  
+  
+  constructor() { }
+  quoteComplete(complete:boolean){
+    this.isComplete.emit(complete);
+    
+  }
+  ngOnInit(): void {
+  }
+
+}
