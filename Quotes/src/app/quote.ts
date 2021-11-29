55bce2f2a4bef3c7d4f95fa89c2efa3e5ef19414
caroline <pcmaunda@gmail.com>
2021-11-29 16:39:13 +0300

    Auto-generated commit [ci skip]

--- Quotes/src/app/quote.ts
+++
@@ -1,8 +0,0 @@
-export class Quote {
-    showAuthor: boolean;
-  static completeDate: Date;
-  static id: number;
-    constructor(public theQuote:string ,public name: string,public author: string, public completeDate: Date){
-      this.showAuthor=false;
-    }
-}
--- README.md
+++
@@ -1,2 +0,0 @@
-# Quotes2
-App for adding quotes to be voted for 
